972a969fc37057445a4596326ded0773dd3ed295
Tung Dao <me@tungdao.com>
2012-06-20 11:15:01 +0700

    Finish chapter 6

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  mail       :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  attr_accessible :email, :name, :password_digest, :password, :password_confirmation
+
+  has_secure_password
+
+  validates :name, presence: true,
+                   length: { maximum: 50 }
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  before_save do |user|
+    user.email = email.downcase
+  end
+end
