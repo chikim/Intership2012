0a6ad71439adadacc1a51cfd765246ba7de7080e
ducvan0212 <ducvan0212@gmail.com>
2012-06-21 16:23:43 +0700

    21-6-2012. Chapter 7

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name "van"
+		email "van@gmail.com"
+		password "vanpassword"
+		password_confirmation "vanpassword"
+	end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,9 +18,9 @@ describe User do
   end
   
   subject { @user }
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
+  #it { should respond_to(:name) }
+  #it { should respond_to(:email) }
+  #it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should be_valid }
