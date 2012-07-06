43c1e57f1c2b5bf2475037d9f28bbf4d5844d40a
ducvan0212 <ducvan0212@gmail.com>
2012-07-06 09:29:01 +0700

    Done tutorial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
   	if signed_in?
-  		@micropost = current_user.microposts.build if signed_in?
+  		@micropost = current_user.microposts.build
   		@feed_items = current_user.feed.paginate(page: params[:page])
   	end
   end
@@ -14,7 +14,7 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
-  
+
   def contact
   end
 end
